7846f5a10cb3f4572c0069cc800118ed97b80f17
Tomás <trebolledo12@gmail.com>
2024-12-06 18:52:51 -0300

    sincronización del archivo con el repositorio en git hub

---
+++ src/test/java/com/apirest/challenge/AluraChallengeApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallengeApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
